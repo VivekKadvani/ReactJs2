11badadf536a7df30e35e3d0674c4a434b4b1101
Vivek Kadvani <vivek.k@simformsolutions.com>
2023-04-20 12:22:10 +0530

    add the code of react useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react'
 function App() {
-  return (
+  
+  const [num , setNum] =useState(1)
+  function inc(){
+    setNum(num+1);
+  }
+  function dec(){
+    if(num!=0)
+    setNum(num-1)
+  }
+
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='number'>{num}</h1>
+      <div className='btns'>
+        <button className='btn' onClick={inc}>Increment</button>
+        <button className='btn' onClick={dec}>Decrement</button>
+      </div>
     </div>
   );
 }
